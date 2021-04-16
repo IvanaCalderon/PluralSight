a587e6b2ba7d5a86743f96a8e458a5ca04d9dc92
Ivana Calderon <ivanacalderonp@gmail.com>
2021-04-15 23:26:54 -0400

    First commit

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Ivana's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+
+            
+            var stats  = book.GetStatistics();
+            
+            System.Console.WriteLine($"The avarage grade is {stats.Average:N1}");
+            System.Console.WriteLine($"The Max grade is {stats.Hihg}");
+            System.Console.WriteLine($"The Min grade is {stats.Low}"); 
+            System.Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+    }
+}
---
+++ gradebook/src/GradeBook/Statistics.cs
@@ -0,0 +1,10 @@
+namespace GradeBook
+{
+    public class Statistics
+    {
+        public double Average;
+        public double Hihg;
+        public double Low;
+        public char Letter;
+    }
+}
