f863ca0f15a63def1586d0e2d7bd7f75c8d7fcc0
Ivana Calderon <ivanacalderonp@gmail.com>
2021-04-23 07:39:50 -0400

    Adding a new project

---
+++ ACM/ACM.BL/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+        public string FirstName { get; set; }
+        public string EmailAddress { get; set; }
+        
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + ", " + LastName;
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+
+            }
+        }
+    }
+}
---
+++ ACM/Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,28 @@
+using System;
+using ACM.BL;
+using Xunit;
+
+namespace ACM.BLTest
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+
+            Customer newCustomer = new Customer();
+            newCustomer.FirstName = "Ivana";
+            newCustomer.LastName = "Calderon";
+
+            //-- Act
+
+            var customerFullName = newCustomer.FullName;
+
+            //-- Assert
+
+            Assert.Equal("Ivana, Calderon", customerFullName);
+
+        }
+    }
+}
